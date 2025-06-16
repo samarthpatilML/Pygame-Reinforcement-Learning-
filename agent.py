79df45f82b2d39ceb47b7d07d4ad7d06108826f4
Samarth Patil <spatilsamarth6886@gmail.com>
2025-06-17 02:11:51 +0530

    .

--- agent.py
+++ agent.py
@@ -3,3 +3,21 @@ import random
 import numpy as np
 from game import SnakeGameAI, Direction, Point
 from collections import deque
+
+max_memory = 100_000
+batch_size = 1000
+LR = 0.001
+
+
+
+class Agent:
+    def __init__(self):
+        pass
+    
+    
+    
+    def get-state(self, game):
+        pass
+    
+    def remember(self, state, action, reward, next_state, done):
+        pass
